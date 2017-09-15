da8f48f44886050f3faa2aed2f8b9633be5e297e
Richard Venneman <richardvenneman@me.com>
2017-09-15 17:53:17 +0200

    Rework deletion of attachments trough controller concern

--- lib/trestle/active_storage/controller_concern.rb
+++ lib/trestle/active_storage/controller_concern.rb
@@ -5,18 +5,32 @@ module Trestle
 
       included do
         after_action :set_attachments, only: %i[create update]
+        after_action :delete_attachments, only: %i[update]
       end
 
       protected
 
       def set_attachments
-        instance_params = params[instance] = admin.permitted_params(params)
-        fields = Rails.cache.read(:active_storage_fields) || []
+        instance_params = admin.permitted_params(params)
 
-        fields.each do |field|
+        attachment_fields.each do |field|
           instance.send(field).attach instance_params[field] if instance_params[field].present?
         end
+      end
 
+      def delete_attachments
+        attachment_fields.each do |field|
+          if instance.send("delete_#{field}") == '1'
+            instance.send(field).purge
+          end
+        end
+      end
+
+      def attachment_fields
+        Rails.cache.read(:active_storage_fields) || []
+      end
+
+      def reset_attachments_cache
         Rails.cache.write(:active_storage_fields, [])
       end
     end
