4f03895e6c0d48b1f768fbb923f2089493437bfd
Richard Venneman <richardvenneman@me.com>
2018-04-16 12:40:48 +0200

    Move attachment accessor definition to controller again

--- lib/trestle/active_storage/controller_concern.rb
+++ lib/trestle/active_storage/controller_concern.rb
@@ -4,12 +4,21 @@ module Trestle
       extend ActiveSupport::Concern
 
       included do
+        before_action :define_attachment_accessors, only: [:edit, :update]
         before_action :delete_attachment_params, only: [:create, :update]
         after_action :attach_attachments, only: %i[create update]
         after_action :purge_attachments, only: %i[update]
       end
 
       protected
+
+      def define_attachment_accessors
+        self.instance = admin.find_instance(params)
+
+        admin.active_storage_fields.each do |field|
+          instance.class.send(:attr_accessor, "delete_#{field}")
+        end
+      end
 
       def delete_attachment_params
         admin.active_storage_fields.each { |field| params.delete(field) }
