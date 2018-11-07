858cc474dfc9578b3f74546fae39ffa4c1845d36
Rafael Porto <faielils@gmail.com>
2018-11-07 04:30:58 -0300

    Getting the attachment preview more flexible.

--- lib/trestle/active_storage/field.rb
+++ lib/trestle/active_storage/field.rb
@@ -11,10 +11,10 @@ module Trestle
             locals[:field_name] = name
 
             if attachment.respond_to?(:each)
-              hash[:partial] = 'trestle/active_storage/has_many_field'
+              hash[:partial] = 'trestle/active_storage/has_many/field'
               locals[:attachments] = attachment
             else
-              hash[:partial] = 'trestle/active_storage/has_one_field'
+              hash[:partial] = 'trestle/active_storage/has_one/field'
               locals[:attachment] = attachment
             end
           end
