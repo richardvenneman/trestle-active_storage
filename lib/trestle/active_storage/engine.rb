b311ebeb949fc746cf007c6686ca46f65ee5ab69
Emanuele Barban <58031+McRipper@users.noreply.github.com>
2020-05-07 12:25:50 +0200

    Upgrade dependency to trestle >= 0.9.3 with new controller

--- lib/trestle/active_storage/engine.rb
+++ lib/trestle/active_storage/engine.rb
@@ -3,10 +3,13 @@ module Trestle
     class Engine < Rails::Engine
       config.assets.precompile << 'activestorage.js' << 'trestle/active_storage_fields.js'
 
+      config.to_prepare do
+        Trestle::ResourceController.send(:prepend, Trestle::ActiveStorage::ControllerConcern)
+      end
+
       initializer :extensions do
         Trestle::Resource.send(:include, Trestle::ActiveStorage::Resource)
         Trestle::Resource::Builder.send(:include, Trestle::ActiveStorage::Builder)
-        Trestle::Resource::Controller.send(:include, Trestle::ActiveStorage::ControllerConcern)
       end
     end
   end
