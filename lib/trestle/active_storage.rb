6d875a0aed7ed7dd354b19220248caaab4246d11
Emanuele Barban <58031+McRipper@users.noreply.github.com>
2020-05-29 20:42:34 +0200

    Require relative instead of autoload

--- lib/trestle/active_storage.rb
+++ lib/trestle/active_storage.rb
@@ -1,14 +1,15 @@
-require 'trestle/active_storage/version'
+require_relative 'active_storage/version'
+
 require 'trestle'
 
 module Trestle
   module ActiveStorage
-    extend ActiveSupport::Autoload
 
-    autoload :Builder
-    autoload :ControllerConcern
-    autoload :Resource
+    require_relative "active_storage/builder"
+    require_relative "active_storage/resource"
+    require_relative "active_storage/controller_concern"
+
   end
 end
 
-require 'trestle/active_storage/engine' if defined?(Rails)
+require_relative 'active_storage/engine' if defined?(Rails)
