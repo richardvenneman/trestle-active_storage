496ad300ed9a52c329a36fda1949466b66aabefc
eddygarcas <edugarcas@gmail.com>
2022-03-29 11:56:35 +0200

    Changing Rails version in trestle-active_storage.gemspec to be bigger than 5.2

--- config/initializers/trestle.rb
+++ config/initializers/trestle.rb
@@ -3,7 +3,7 @@ require 'trestle/active_storage/field'
 Trestle.configure do |config|
     config.hook(:javascripts) do
     javascript_include_tag('activestorage') +
-      javascript_include_tag('trestle/active_storage_fields.js')
+    javascript_include_tag('trestle/active_storage_fields.js')
   end
 
   config.form_field :active_storage_field, Trestle::ActiveStorage::Field
